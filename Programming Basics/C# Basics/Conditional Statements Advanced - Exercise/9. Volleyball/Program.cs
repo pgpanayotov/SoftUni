a274bc23c3135b2d5aaf25ef7acb6d721b6f1a3b
Plamen Panayotov <plamenx93@gmail.com>
2020-11-08 19:12:22 +0200

    8.11.2020 work

---
+++ Programming Basics/C# Basics/Conditional Statements Advanced - Exercise/9. Volleyball/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9._Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int YearWeekends = 48;
+
+            // leap" (високосна година) или "normal" (невисокосна).
+            string yearType = Console.ReadLine();
+            // брой празници в годината (които не са събота и неделя).
+            int holidays = int.Parse(Console.ReadLine());
+            // брой уикенди, в които Влади си пътува до родния град.
+            int homeTownWeekends = int.Parse(Console.ReadLine());
+            int weekendsInSofia = YearWeekends - homeTownWeekends;
+
+            double satGamesInSofia = weekendsInSofia * 3.0 / 4.0;
+            double sunGamesInHomeTown = homeTownWeekends;
+            double holidayGamesInSofia = holidays * 2.0 / 3.0;
+
+            double totalGames = satGamesInSofia + sunGamesInHomeTown + holidayGamesInSofia;
+            if (yearType == "leap")
+            {
+                totalGames += totalGames * 0.15;
+            }
+            Console.WriteLine(Math.Floor(totalGames));
+        }
+    }
+}
